a620d1a50db1a052f00491b501daba15f12844ca
Sooho Kang <shkorea1004@gmail.com>
2021-03-09 17:20:14 +0900

    init

--- Shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v "#" | awk " NR % 2 == 0" | sed "s/:*:.*//g" | rev| sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ',' | tr -d ' ' | sed "s/,/, /g" | sed "s/\(.*\),.*/\1./g"

447f2e6895afd38b5678fe4535af8385abbad24d
Sooho Kang <shkorea1004@gmail.com>
2021-03-11 18:56:00 +0900

    *

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,19 +6,19 @@
 /*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 22:27:56 by sookang           #+#    #+#             */
-/*   Updated: 2021/03/11 18:27:49 by sookang          ###   ########.fr       */
+/*   Updated: 2021/03/11 18:55:16 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void ft_putstr(char *str)
 {
 	while (*str)
 		write(1, str++, 1);
 }
 
-void	swap(char **a, char **b)
+void swap(char **a, char **b)
 {
 	char *c;
 
@@ -27,14 +27,14 @@ void	swap(char **a, char **b)
 	*b = c;
 }
 
-int		ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
 	int i;
 
 	i = 0;
 	while (s1[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[i] && s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
@@ -43,14 +43,14 @@ int		ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-void	sort(int argc, char *argv[])
+void sort(int argc, char *argv[])
 {
 	int i;
 
 	i = 1;
 	while (i < argc - 1)
 	{
-		if (ft_strcmp(argv[i], argv[i + 1]) >= 0)
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 		{
 			swap(&argv[i + 1], &argv[i]);
 			i = 0;
@@ -59,7 +59,7 @@ void	sort(int argc, char *argv[])
 	}
 }
 
-int		main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 

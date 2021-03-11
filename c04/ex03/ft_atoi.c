2d9b5a5a7140ca2ec1576cdc804dc1af8dba6807
Sooho Kang <shkorea1004@gmail.com>
2021-03-11 23:35:09 +0900

    ex05*

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,13 +6,15 @@
 /*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 17:06:09 by sookang           #+#    #+#             */
-/*   Updated: 2021/03/11 20:25:15 by sookang          ###   ########.fr       */
+/*   Updated: 2021/03/11 21:40:48 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		is_negative(char c)
+int		is_sign(char c)
 {
-	return (c == '-' ? 1 : 0);
+	if (c == '-' || c == '+')
+		return (1);
+	return (0);
 }
 
 int		is_number(char c)
@@ -40,25 +42,25 @@ int		ft_atoi(char *str)
 	int i;
 	int num;
 	int neg_flag;
-	int num_flag;
 
 	i = 0;
 	neg_flag = 1;
 	num = 0;
 	while (is_space(str[i]))
 		i++;
-	while (str[i] && (num_flag || is_number(str[i])))
+	while (is_sign(str[i]))
+	{
+		if (str[i++] == '-')
+			neg_flag *= -1;
+	}
+	while (str[i] && is_number(str[i]))
 	{
 		if (is_space(str[i]))
 			return (0);
-		if (is_negative(str[i]))
-			neg_flag *= -1;
-		if (is_number(str[i]))
-		{
-			num *= 10;
-			num += (str[i] - 48);
-			num_flag = 0;
-		}
+		if (!is_number(str[i]))
+			return (0);
+		num *= 10;
+		num += (str[i] - 48);
 		i++;
 	}
 	return (neg_flag * num);
--- c04/ex03/main.c
+++ c04/ex03/main.c
@@ -6,7 +6,7 @@ int main()
 	printf("%d\n", ft_atoi("---++1234a b567s444R"));
 	printf("%d\n", ft_atoi("---+1234ab567s444R"));
 	printf("%d\n", ft_atoi("  --- +-+2147483647"));
-	printf("%d\n", ft_atoi("----++2147483647"));
+	printf("%d\n", ft_atoi("----d++2147483647"));
 	printf("%d\n", ft_atoi("---+-+1234ab567s444R"));
 	printf("%d\n", ft_atoi("---+-+1234ab567s444R"));
 }

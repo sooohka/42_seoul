f48559d81c6c1afe7766650bfef7c72423079be1
Sooho Kang <shkorea1004@gmail.com>
2021-03-17 02:14:39 +0900

    *

--- c09/ex02/ft_split.c
+++ c09/ex02/ft_split.c
@@ -6,11 +6,71 @@
 /*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 20:22:17 by sookang           #+#    #+#             */
-/*   Updated: 2021/03/16 20:22:24 by sookang          ###   ########.fr       */
+/*   Updated: 2021/03/17 02:11:49 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char **ft_split(char *str, char *charset)
+#include <stdlib.h>
+
+int		is_charset(char c, char *charset)
 {
-	
+	int i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int		get_strs_length(char *str, char *charset)
+{
+	int count;
+	int i;
+
+	i = 0;
+	count = 0;
+	while (is_charset(str[i], charset))
+		i++;
+	while (str[i])
+	{
+		if (is_charset(str[i], charset) && !is_charset(str[i + 1], charset) \
+		&& str[i + 1])
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count + 1);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**strs;
+	int		i;
+	int		j;
+	int		count;
+
+	i = -1;
+	count = get_strs_length(str, charset);
+	if (!(strs = (char **)malloc(sizeof(char *) * (count + 1))))
+		return (NULL);
+	while (++i < count)
+	{
+		while (is_charset(*str, charset))
+			str++;
+		j = 0;
+		while (!is_charset(str[j], charset))
+			j++;
+		strs[i] = (char *)malloc(sizeof(char) * (j + 1));
+		j = 0;
+		while (!is_charset(*str, charset))
+			strs[i][j++] = *(str++);
+		strs[i][j] = 0;
+	}
+	strs[i] = 0;
+	return (strs);
 }
--- c09/ex02/main.c
+++ c09/ex02/main.c
@@ -6,13 +6,30 @@
 /*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 20:22:29 by sookang           #+#    #+#             */
-/*   Updated: 2021/03/16 20:23:36 by sookang          ###   ########.fr       */
+/*   Updated: 2021/03/17 02:08:59 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char **ft_split(char *str, char *charset);
+int    get_strs_length(char *str, char *charset);
+
+#include <stdio.h>
 
 int main()
 {
-	
+	char   str[] = "         hello wor,ld b,ye,     4, , , ,,home  i";
+	char   charset[] = " , ";
+	char **strs;
+	int    i;
+
+	i = 0;
+	strs = ft_split(str, charset);
+	while (strs[i])
+	{
+		printf("%s", strs[i]);
+		printf("\n");
+		i++;
+	}
+
+	return (0);
 }

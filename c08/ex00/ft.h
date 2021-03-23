edbd99acbe1723956c0a6e480c8712fbfa876017
soohoKang <67774824+sooho-Kang@users.noreply.github.com>
2021-03-23 11:51:17 +0900

    C08 (#11)

---
+++ c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 11:06:17 by sookang           #+#    #+#             */
+/*   Updated: 2021/03/16 11:12:35 by sookang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void		ft_putchar(char c);
+void		ft_swap(int *a, int *b);
+void		ft_putstr(char *str);
+int			ft_strlen(char *str);
+int			ft_strcmp(char *s1, char *s2);
+
+#endif

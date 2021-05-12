e31c65b42f120d794c5b168fc6d7adfbaf5d9326
sooho-kang <shkorea1004@gmail.com>
2021-05-12 12:37:22 +0900

    ft_itoa.c

--- libft/libft/ft_itoa.c
+++ libft/libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: sookang <sookang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 14:42:38 by sookang           #+#    #+#             */
-/*   Updated: 2021/05/11 15:03:20 by sookang          ###   ########.fr       */
+/*   Updated: 2021/05/12 12:1 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,37 @@ char *ft_itoa(int n)
 	int   temp;
 	char *str;
 	int   digit;
-	char *loc;
+	int   sign;
 
+	sign = 1;
 	digit = 0;
 	temp = n;
 	if (n == 0)
 		return ft_strdup("0");
-	while (temp > 0)
+	if (n < 0)
+	{
+		sign *= -1;
+		digit++;
+	}
+	while (temp != 0)
 	{
 		temp /= 10;
 		digit++;
 	}
 	if (!(str = (char *) malloc((digit + 1) * sizeof(char))))
 		return (NULL);
-	loc = str;
-	while (n > 0)
+	str[digit--] = 0;
+	while (n != 0)
 	{
-		*loc = n % 10 + 48;
+		str[digit--] = ((n % 10) * sign) + 48;
 		n /= 10;
-		loc++;
 	}
+	if (sign < 0)
+		str[0] = '-';
 	return (str);
 }
 
 int main()
 {
-	printf("%s", ft_itoa(123));
+	printf("%s", ft_itoa(-2134515)) ;
 }

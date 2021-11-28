cc285947e08b2f998dccb95cabe244f9416c31c4
sooho-kang <shkorea1004@gmail.com>
2021-11-28 17:58:22 +0900

    printf: fix libft

--- printf/libft/src/ft_substr.c
+++ printf/libft/src/ft_substr.c
@@ -6,11 +6,17 @@
 /*   By: sookang <sookang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 20:08:28 by sookang           #+#    #+#             */
-/*   Updated: 2021/11/28 17:45:41 by sookang          ###   ########.fr       */
+/*   Updated: 2021/11/28 17:57:41 by sookang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*ft_rt(char *sub, int i)
+{
+	sub[i] = 0;
+	return (sub);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -28,14 +34,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!sub)
 		return (NULL);
 	if (start > ft_strlen(s))
-	{
-		sub[i] = 0;
-		return (sub);
-	}
+		return (ft_rt(sub, i));
 	str += start;
 	while (i < len && str[i])
 	{
-		sub[i] = str[i++];
+		sub[i] = str[i];
+		i++;
 	}
 	sub[i] = 0;
 	return (sub);
